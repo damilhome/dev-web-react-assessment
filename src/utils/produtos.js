df6a3d572c13f03090a475eeae77c29404a6f9ad
Danilo Almeida Milhome <damilhome@gmail.com>
2026-06-13 14:16:08 -0300

    aperfeicoando ex7

--- src/utils/produtos.js
+++ src/utils/produtos.js
@@ -6,8 +6,8 @@ const produtos = new Array(100).fill().map(() => ({
   descricao: faker.commerce.productDescription(),
   adjetivo: faker.commerce.productAdjective(),
   preco: faker.commerce.price({ min: 10, max: 2000, dec: 2, symbol: "R$ " }),
-  imagem: faker.image.urlLoremFlickr({
-    category: "product",
+  imagem: faker.image.url({
+    category: "commerce",
   }),
   avaliacao: faker.number.float({
     min: 1,
